6a5d3b24650c3b74a414a28ea31f7bad4c77a4ee
Riley Peuter <88885012+RileyPeuter@users.noreply.github.com>
2024-08-08 10:21:43 +1000

    Create Objective.cs
    initialCom

---
+++ Objective.cs
@@ -0,0 +1,185 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum ObjectiveModifier
+{
+    None, 
+    GreaterThan
+}
+
+public class Objective
+{
+    //###MemberVariables###
+    ObjectiveModifier modifier = ObjectiveModifier.None;
+    int modifierThreshold;
+
+    public string objectiveID;
+    string description;
+    string stringData;
+    int maxCompletions;
+    int currentCompletions = 0;
+
+    BattleEventType objectiveType;
+    
+    string subject = null;
+    string verb = null;
+    string target = null;
+    string result = null;
+
+    //Non Visible objectives are used for triggers
+    bool visible = true;
+
+    public bool completed = false;
+
+    GameObject parent;
+    Text descriptionText;
+    Text completedText;
+    float yOffset;
+
+    //###Utilities###
+    public bool checkObjective(BattleEventType nBattleEventType, string nSubject = null, string nVerb = null, string nTarget = null, string nResult = null)
+    {
+
+        //This block just checks if the event checks if something is equal to the objecitve
+        if(objectiveType != nBattleEventType){return false;}
+        if(subject != null && subject != nSubject){return false;}
+        if(verb != null && verb != nVerb){return false;}
+        if(target != null && target != nTarget) {return false;}
+        if(result != null && result != nResult){return false;}
+             
+        if (testModifier(modifier, nResult, modifierThreshold))
+        {
+            return true;
+        }
+             
+        return true;
+    }
+
+    //Adds more logic to objectives like "Deal MORE than 6 damage"
+    public bool testModifier(ObjectiveModifier objectiveModifier, string result, int threshHold)
+    {
+        switch (objectiveModifier)
+        {
+            case ObjectiveModifier.None:
+                return true;
+
+            case ObjectiveModifier.GreaterThan:
+                if(int.Parse(result) >= threshHold){
+
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+
+    void checkVisible()
+    {
+        parent.SetActive(visible);
+    }
+
+    //###Setters###
+    //These are writen like this so you can easily chain them together
+    public Objective addSubject(string nSubject)
+    {
+        subject = nSubject;
+        return this;
+    }
+
+    public Objective addVerb(string nVerb)
+    {
+        verb = nVerb;
+        return this;
+    }
+    public Objective addTarget(string nTarget)
+    {
+        target = nTarget;
+        return this;
+    }
+
+    public Objective addParent(GameObject nParent)
+    {
+        parent = nParent;
+        return this;
+    }
+    public Objective addOffset(float nOffset)
+    {
+        yOffset = nOffset;
+        return this;
+    }
+
+    public Objective addDescription(string nDescription)
+    {
+        description = nDescription;
+        return this;
+    }
+
+    public Objective addModifier(ObjectiveModifier nObjectiveModifer, int nThreshold)
+    {
+        modifier = nObjectiveModifer;
+        modifierThreshold = nThreshold; 
+        return this;
+    }
+
+    //###Utilities###
+    public void setInfo(float offset)
+    {
+        parent.transform.Translate(0, offset, 0);
+        foreach (Text text in parent.GetComponentsInChildren<Text>())
+        {
+            if (text.gameObject.name == "uI_ObjectiveDescription_Text")
+            {
+                descriptionText = text;
+            }
+            if (text.gameObject.name == "uI_ObjectiveAmount_Text")
+            {
+                completedText = text;
+            }
+        }
+    }
+
+    public void setText()
+    {
+        if (!visible) { return; }
+
+        descriptionText.text = description;
+        completedText.text = currentCompletions.ToString() + "/" + maxCompletions.ToString();
+        if (currentCompletions >= maxCompletions)
+        {
+            descriptionText.color = Color.green;
+            completedText.color = Color.green;
+        }
+    }
+
+    //Used for when you get closer to an objective, returns true if it's completed
+    public bool increment(int amount = 1)
+    {
+        currentCompletions += amount;
+        return (currentCompletions >= maxCompletions);
+    }
+
+    //###Constructors###
+    public Objective(string nID, int nMaxCompletion, BattleEventType nObjectiveType)
+    {
+        objectiveID = nID;
+        maxCompletions = nMaxCompletion;
+        objectiveType = nObjectiveType;
+    }
+
+    public Objective(string nObjectiveID, string nDescription, int nMaxCompletion, BattleEventType nObjectiveType, GameObject nParent, float offset, int nCurrentCompletion = 0)
+    {
+        objectiveID = nObjectiveID;
+        description = nDescription;
+        maxCompletions = nMaxCompletion;       
+        objectiveType = nObjectiveType;
+        currentCompletions = nCurrentCompletion;
+        parent = nParent;
+
+        setInfo(offset);
+        setText();
+    }
+}
