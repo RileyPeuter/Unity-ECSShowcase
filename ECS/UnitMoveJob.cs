1f88b55524ef9d0104125157d82d53c1d81254e5
Riley Peuter <88885012+RileyPeuter@users.noreply.github.com>
2024-08-08 12:49:40 +1000

    Create UnitMoveJob.cs

---
+++ ECS/UnitMoveJob.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.Entities;
+using UnityEngine;
+using Unity.Transforms;
+using Unity.Burst;
+using Unity.Jobs;
+using Unity.VisualScripting;
+using Unity.Mathematics;
+
+[BurstCompile]
+public partial struct UnitMoveJob : IJobEntity
+{
+    public float deltaTime;
+    private void Execute(ref Unit unit, ref LocalTransform tranform)
+    {
+        switch (unit.aIMode)
+        {
+            case AIMode.AttackMove:
+                tranform.Translate(new float3(1 * unit.moveSpeed * deltaTime, 0, 0));
+                break;
+
+            case AIMode.HoldPosition:
+                //Doesn't move
+                break;
+
+            case AIMode.RunAway:
+                tranform.Translate(new float3(-1 * unit.moveSpeed * deltaTime, 0, 0));
+                break;
+        }
+    }
+}
